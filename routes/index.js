cb5f66672a9ca1493408090b35f4c7e2486dfe13
张钰浩 <32158308@qq.com>
2017-05-31 09:21:12 +0800

    不使用通用路由

--- routes/index.js
+++ routes/index.js
@@ -7,28 +7,29 @@ router.get('/', function(req, res, next) {
 });
 
 
+// 不使用通用路由
 // 通用路由
-router.route('/api/:service/:method').all(function(req, res, next) {
-    // 获取serviceName和methodName
-    let serviceName = req.params.service;
-    let methodName = req.params.method;
+// router.route('/api/:service/:method').all(function(req, res, next) {
+//     // 获取serviceName和methodName
+//     let serviceName = req.params.service;
+//     let methodName = req.params.method;
 
-    // 导入service
-    let Service = require('../services/'+serviceName);
-    let service = Service.getInstance();
+//     // 导入service
+//     let Service = require('../services/'+serviceName);
+//     let service = Service.getInstance();
 
-    console.log(req.body);
-    console.log(req.query);
-    // 获取param
-    let param = Object.assign({}, req.body, req.query);
+//     console.log(req.body);
+//     console.log(req.query);
+//     // 获取param
+//     let param = Object.assign({}, req.body, req.query);
 
-    // 调用
-    let result = service[methodName](param);
+//     // 调用
+//     let result = service[methodName](param);
 
-    // 返回数据
-    res.json(result);
+//     // 返回数据
+//     res.json(result);
 
-})
+// });
 
 
 module.exports = router;
