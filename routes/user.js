c91d987df9a2a7896729255013f996a4ae5289b7
张钰浩 <32158308@qq.com>
2017-07-21 14:49:26 +0800

    test

--- routes/user.js
+++ routes/user.js
@@ -74,8 +74,8 @@ router.get('/wxlogin', function(req, response, next){
 // 测试用，获取session中的user
 router.get('/getSessionUser', function(req, res, next){
     console.log('-----------------');
-    console.log(req.session.user);
-    res.json(req.session.user);
+    console.log(req.sessionID);
+    res.json(req.sessionID);
 });
 
 // 根据小程序中获取的用户信息，对服务端进行登录
